2c163bd0c87faaae5a00f2b709f0ebe2bbd88ef7
XxshiftxX <qjfrntop12@naver.com>
2018-09-02 20:58:43 +0900

    init

---
+++ LessonObserver/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.CognitiveServices.Speech;
+
+namespace LessonObserver
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Initialize()
+        {
+            var factory = SpeechFactory.FromSubscription("9097559d41b44e09895fedc53e1a2c69", "eastasia");
+        }
+    }
+}
---
+++ STTTest/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.CognitiveServices.SpeechRecognition;
+using System;
+using System.Linq;
+using System.Threading;
+
+namespace helloworld
+{
+    class Program
+    {
+        static AutoResetEvent fre = new AutoResetEvent(false);
+        static MicrophoneRecognitionClient l;
+
+        static void Main()
+        {
+            var speechRecogeMode = SpeechRecognitionMode.LongDictation;
+            var lang = "en-us";
+            var subkey = "c060c1c953504758ae24a0e6f17f7d71";
+            var cur = string.Empty;
+            var r = string.Empty;
+            l = SpeechRecognitionServiceFactory.CreateMicrophoneClient(speechRecogeMode, lang, subkey);
+            l.OnResponseReceived += (sender, e) =>
+            {
+                Console.Clear();
+
+                if (e.PhraseResponse.Results.Count() > 0)
+                {
+                    var res = e.PhraseResponse.Results.Last().DisplayText;
+
+                    if (res.Contains("학생"))
+                        cur += res + '\n';
+                    else
+                        r += res + '\n';
+                }
+                Console.WriteLine(cur);
+                Console.WriteLine();
+                
+                Console.WriteLine(r);
+            };
+
+            l.StartMicAndRecognition();
+        }
+    }
+}
